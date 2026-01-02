8de8aeb8f792e9f8497f7e965d7a37c2f605a8b6
jchhabra1011 <jatinkumarchhabra1011@gmail.com>
2026-01-02 14:25:35 +0530

    name update

---
+++ 09-mysql/challenges/name-formatter.sql
@@ -0,0 +1,15 @@
+SELECT
+    CONCAT(UPPER(SUBSTRING_INDEX(name, ' ', -1)), ', ', 
+           SUBSTRING_INDEX(name, ' ', 1)) AS formatted_name,
+
+    LOWER(CONCAT(
+        SUBSTRING_INDEX(name, ' ', 1), '.', 
+        SUBSTRING_INDEX(name, ' ', -1), 
+        '@company.com'
+    )) AS email,
+
+    CONCAT(
+        UPPER(LEFT(SUBSTRING_INDEX(name, ' ', 1), 1)),
+        UPPER(LEFT(SUBSTRING_INDEX(name, ' ', -1), 1))
+    ) AS initials
+FROM employees;
